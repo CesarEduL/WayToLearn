a81c75f86e670b2bb348de651316467590916417
Cesar Eduardo Lizama Camero <U20201545@utp.edu.pe>
2024-05-07 13:43:19 -0500

    Books buttons putted

--- lib/main.dart
+++ lib/main.dart
@@ -21,22 +21,29 @@ class MyApp extends StatelessWidget {
               ),
               SizedBox(height: 20),
               // Botones
-              CustomButton(
-                image:
-                    'assets/images/booksolid.png',
-                text: 'Cuento 1',
+              Padding(
+                padding: EdgeInsets.only(right: 120), // Relleno a la derecha
+                child: CustomButton(
+                  icon: Icons.book,
+                  text: 'Cuento 1',
+                ),
               ),
-              SizedBox(height: 10),
-              CustomButton(
-                image:
-                    'assets/images/booksolid.png',
-                text: 'Cuento 2',
+              SizedBox(height: 20), // Espacio vertical entre botones
+              Padding(
+                padding: EdgeInsets.only(
+                    left: 140), // Relleno a la izquierda y a la derecha
+                child: CustomButton(
+                  icon: Icons.book,
+                  text: 'Cuento 2',
+                ),
               ),
-              SizedBox(height: 10),
-              CustomButton(
-                image:
-                    'assets/images/booksolid.png',
-                text: 'Cuento 3',
+              SizedBox(height: 20), // Espacio vertical entre botones
+              Padding(
+                padding: EdgeInsets.only(right: 120), // Relleno a la izquierda
+                child: CustomButton(
+                  icon: Icons.book,
+                  text: 'Cuento 3',
+                ),
               ),
             ],
           ),
@@ -47,36 +54,43 @@ class MyApp extends StatelessWidget {
 }
 
 class CustomButton extends StatelessWidget {
-  final String image;
+  final IconData icon;
   final String text;
 
-  CustomButton({required this.image, required this.text});
+  CustomButton({required this.icon, required this.text});
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      width: 150,
-      height: 150,
+      width: 120,
+      height: 120,
       child: ElevatedButton(
         onPressed: () {
           // Acción cuando se presiona el botón
         },
         style: ButtonStyle(
-          backgroundColor: MaterialStateProperty.all<Color>(Color.fromARGB(174, 73, 147, 221)),
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15.0),
+            ),
+          ),
+          backgroundColor: MaterialStateProperty.all<Color>(
+            Color.fromARGB(174, 73, 147, 221),
+          ),
         ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Image.asset(
-              image,
-              width: 80,
-              height: 80,
+            Icon(
+              icon,
+              size: 60,
+              color: Colors.white,
             ),
-            SizedBox(height: 10),
+            SizedBox(height: 5),
             Text(
               text,
               style: TextStyle(
-                fontSize: 16,
+                fontSize: 14,
                 color: Colors.white,
               ),
             ),
---
+++ lib/pages/pagina02.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Pagina02 extends StatelessWidget {
+  const Pagina02({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
